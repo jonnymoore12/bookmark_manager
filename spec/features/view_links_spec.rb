562dbf0c404fae32b3e12e892a70986a491fa1d0
Adam <adamjbrowner@gmail.com>
2016-07-27 15:28:07 +0100

    Challenge 16

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,4 +1,8 @@
 feature '#viewing_links' do
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
   scenario 'user views link on homepage' do
     Link.create(title: 'google', url: 'www.google.com' )
 
@@ -7,6 +11,14 @@ feature '#viewing_links' do
 
     within('ul#links') do
       expect(page).to have_content 'google'
+    end
+  end
+
+  scenario "Filter by bubbles tags" do
+    visit '/tags/bubbles'
+    within "ul#links" do
+      expect(page).not_to have_content("Makers Academy")
+      expect(page).to have_content("This is Zombocom")
     end
   end
 end
